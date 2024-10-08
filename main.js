101312688697feddf8dbadda164b38ffa7df8e82
ZTPedroDaniel <147394862+ZTPedroDaniel@users.noreply.github.com>
2024-10-08 08:50:14 -0300

    .

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ function criaCartao(pergunta, resposta){
     let conteudo = document.getElementById('conteudo')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-    console.log(cartao)
+
+    cartao.innerHTML = `
+        <div class="cartao--conteudo">
+           <div class="cartao--pergunta"><p>Quais são os principais animatronics?</p></div>
+           <div class="cartao--resposta"><p>Freddy, Bonnie, Chica e Foxy</p></div>
+        </div>
+    `
+
+    console.log()
+
 }
 
