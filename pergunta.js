b016f187a5f67cedf64dc83fedafb9380060ac41
ZTPedroDaniel <147394862+ZTPedroDaniel@users.noreply.github.com>
2024-10-29 08:27:48 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -3,10 +3,22 @@ criaCartao(
     "Freddy, Bonnie, Chica e Foxy"
 )
 criaCartao(
-    "Quais são os principais Animatronics?",
-    "Freddy, Bonnie, Chica e Foxy"
+    "Desde quando a Fazbear atua?",
+    "Desde 1983"
 )
 criaCartao(
-    "Quais são os principais Animatronics?",
-    "Freddy, Bonnie, Chica e Foxy"
+    "Quem foram os fundadores da Fazbear Entreteriments™?",
+    "William Afton e Henry Emily"
+)
+criaCartao(
+    "O que são os rumores envolvidos na fazbear?",
+    "São apenas lendas urbanas"
+)
+criaCartao(
+    "Como eu contrato os serviços da Fazbear?",
+    "Ligue para o numero 000-1987-83"
+)
+criaCartao(
+    "O que fazer se meu filho desaparecer?,
+    "A Fazbear Entreteriments™ não se responsabiliza por objetos ou crianças desaparecidas nos nossos estabelecimentos."
 )
